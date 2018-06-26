12377b12ccd1041af751efe8600b0dec322d461f
MrRootick <31653337+MrRootick@users.noreply.github.com>
2018-06-26 23:32:12 +0300

    1

--- comparison.py
+++ comparison.py
@@ -38,7 +38,7 @@ def analys(brige,y,i,x):
                 sell=float(abc[2])
                 proc = (sell-buy) /(buy*0.01)
                 if buy!=None or sell!=None:
-                    if proc >= 3:
+                    if proc >= 0:
                         if buy < sell and (buy*0.01)<(sell-buy):
 
                             return (abc[0] + '\n' +
